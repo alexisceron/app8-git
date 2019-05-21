1751647a011ae36adaa680c768096019d04a0dfb
Alexis <alexis00sam66@gmail.com>
2019-05-21 13:32:52 -0500

    2

--- js/acciones.js
+++ js/acciones.js
@@ -18,7 +18,7 @@ function getPosition(){
 		maximumAge:3600000
 	}
 	
-	var watchID=navigatior.geolocation.getCurrentPosition(onSuccess, onError, options);
+	var watchID=navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
 	function onSuccess(position){
 		
 		alert('Latitude:'	+position.coords.latitude	+'\n'+
